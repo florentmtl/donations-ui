589d83718439c820f91e3c958ac490e6acbdeb8a
Florent Allard <florent.allard@polymtl.ca>
2024-05-01 09:22:18 -0400

    feat(app): add select for rows per page value

--- src/components/MultiPageFooter.jsx
+++ src/components/MultiPageFooter.jsx
@@ -1,22 +1,22 @@
 export function MultiPageFooter({ rowsPerPage, setRowsPerPage, pageNum, setPageNum, numTotalElements }) {
-  // for rows per page define dropdown with predefined values
-
   const handleRows = (e) => {
-    setRowsPerPage(e.target.value);
+    // aggressive solution
+    setPageNum(1);
+    setRowsPerPage(parseInt(e.target.value));
   };
 
   return (
     <div className="d-flex flex-row justify-content-center align-self-center">
       <div className="me-3 d-flex align-self-center">
-        <label htmlFor="donations-rows-per-page">Rows per page </label>
-        <input
-          style={{ width: '50px' }}
-          type="number"
-          name="rows-per-page"
-          id="donations-rows-per-page"
-          value={rowsPerPage}
-          onChange={handleRows}
-        />
+        <label className="me-2" htmlFor="select-pprows-donations">
+          Rows per page
+        </label>
+        <select name="select-pprows" id="select-pprows-donations" value={rowsPerPage} onChange={handleRows}>
+          <option value="10">10</option>
+          <option value="30">30</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
       </div>
       <div className="me-3 d-flex align-self-center">
         {1 + (pageNum - 1) * rowsPerPage}-{Math.min(pageNum * rowsPerPage, numTotalElements)} of {numTotalElements}
