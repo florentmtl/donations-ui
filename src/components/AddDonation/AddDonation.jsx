34b1da6985789d73bec560ebf9feccc9320ac015
Florent Allard <florent.allard@polymtl.ca>
2024-05-02 11:51:21 -0400

    feat(app): submit form data to backend

--- src/components/AddDonation/AddDonation.jsx
+++ src/components/AddDonation/AddDonation.jsx
@@ -1,9 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useState } from 'react';
 
 export function AddDonation() {
-  const handleSubmit = () => {};
+  const [inputs, setInputs] = useState({ type: 'Manual' });
+  const [isAnonymous, setIsAnonymous] = useState(false);
+  const [donationDate, setDonationDate] = useState(new Date());
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const fullInfo = { ...inputs, createdAtUtc: donationDate.getTime(), isAnonymous: isAnonymous };
+    fetch('http://localhost:4000/donations', {
+      method: 'POST',
+      body: JSON.stringify(fullInfo),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .catch((err) => {
+        console.log(err);
+      });
+    navigate('/');
+  };
 
   return (
     <div className="container" style={{ maxWidth: '700px' }}>
@@ -17,10 +44,25 @@ export function AddDonation() {
         <form onSubmit={handleSubmit}>
           <div className="mb-3 row">
             <div className="col">
-              <input className="form-control" type="text" name="first-name" id="first-name" placeholder="First name" />
+              <input
+                className="form-control"
+                type="text"
+                name="firstName"
+                id="first-name"
+                placeholder="First name"
+                value={inputs.firstName || ''}
+                onChange={handleChange}
+              />
             </div>
             <div className="col">
-              <input className="form-control" type="text" name="last-name" id="last-name" placeholder="Last name" />
+              <input
+                className="form-control"
+                type="text"
+                name="lastName"
+                id="last-name"
+                placeholder="Last name"
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="row mb-3">
@@ -28,7 +70,7 @@ export function AddDonation() {
               <label htmlFor="amount-form">Amount</label>
               <div className="input-group">
                 <span className="input-group-text">$</span>
-                <input type="text" id="amount-form" className="form-control" />
+                <input className="form-control" type="number" id="amount-form" name="amount" onChange={handleChange} />
                 <span className="input-group-text">.00</span>
               </div>
             </div>
@@ -36,7 +78,13 @@ export function AddDonation() {
               <label htmlFor="refunded-amount-form">Refunded amount</label>
               <div className="input-group">
                 <span className="input-group-text">$</span>
-                <input type="text" id="refunded-amount-form" className="form-control" />
+                <input
+                  className="form-control"
+                  type="number"
+                  id="refunded-amount-form"
+                  name="refundedAmount"
+                  onChange={handleChange}
+                />
                 <span className="input-group-text">.00</span>
               </div>
             </div>
@@ -46,9 +94,15 @@ export function AddDonation() {
               Type
             </label>
             <div className="col-sm-9">
-              <select className="form-select" name="type-donation" id="type-donation">
-                <option value="charge">Charge</option>
-                <option value="manual">Manual</option>
+              <select
+                className="form-select"
+                name="type"
+                id="type-donation"
+                value={inputs.type}
+                onChange={handleChange}
+              >
+                <option value="Charge">Charge</option>
+                <option value="Manual">Manual</option>
               </select>
             </div>
           </div>
@@ -57,7 +111,7 @@ export function AddDonation() {
               Date
             </label>
             <div className="col-sm-9">
-              <DatePicker id="date-donation" />
+              <DatePicker id="date-donation" selected={donationDate} onChange={(date) => setDonationDate(date)} />
             </div>
           </div>
           <div className="mb-3 row">
@@ -65,7 +119,14 @@ export function AddDonation() {
               Thank you comment
             </label>
             <div className="col-sm-9">
-              <textarea className="form-control" name="thank-you" id="thank-you" rows="2" />
+              <textarea
+                className="form-control"
+                name="thankYouComment"
+                id="thank-you"
+                rows="2"
+                value={inputs.thankYouComment || ''}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="mb-3 row">
@@ -73,11 +134,25 @@ export function AddDonation() {
               Company name
             </label>
             <div className="col-sm-9">
-              <input className="form-control" type="text" name="company-name" id="company-name" />
+              <input
+                className="form-control"
+                type="text"
+                name="companyName"
+                id="company-name"
+                value={inputs.companyName || ''}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="mb-3 form-check">
-            <input className="form-check-input" type="checkbox" value="" id="anonymous-donation" />
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="anonymous-donation"
+              name="isAnonymous"
+              value={isAnonymous}
+              onChange={(e) => setIsAnonymous(e.target.checked)}
+            />
             <label className="form-check-label" htmlFor="anonymous-donation">
               Anonymous donation
             </label>
